1e088d6e30523a856e18a420f456f9444269f152
Christian Deckert <c.deckert@me.com>
2014-03-09 13:35:04 +0100

    less debugging

--- HDD-Benchmark/Skippy.cpp
+++ HDD-Benchmark/Skippy.cpp
@@ -1,4 +1,24 @@
-i//perror("write");   
+/* 
+ * File:   Skippy.cpp
+ * Author: christiandeckert
+ * 
+ * Created on March 8, 2014, 8:35 PM
+ */
+
+#include "Skippy.h"
+
+#include <stdio.h>
+
+Skippy::Skippy(char *theAddress): Benchmark(theAddress){
+    
+}
+
+
+
+void Skippy::execute(int iteration)
+{
+    lseek64(getFd(), (getSingleSector() * iteration), SEEK_CUR);
+    write(getFd(), &buffer,  sizeof(buffer));
 }
 
 
