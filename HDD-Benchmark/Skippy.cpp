2b9d987cb079be578abe90bceb61ac6a04dc6371
Christian Deckert <c.deckert@me.com>
2014-03-09 13:33:28 +0100

    formatting

--- HDD-Benchmark/Skippy.cpp
+++ HDD-Benchmark/Skippy.cpp
@@ -1,30 +1,4 @@
-/* 
- * File:   Skippy.cpp
- * Author: christiandeckert
- * 
- * Created on March 8, 2014, 8:35 PM
- */
-
-#include "Skippy.h"
-
-#include <stdio.h>
-
-Skippy::Skippy(char *theAddress): Benchmark(theAddress){
-    
-}
-
-
-
-void Skippy::execute(int iteration)
-{
-    //
-	
-    
-    lseek64(getFd(), (getSingleSector() * iteration), SEEK_CUR);
-    write(getFd(), &buffer,  sizeof(buffer));
-    //perror("write");
-
-        
+i//perror("write");   
 }
 
 
--- HDD-Benchmark/build/Release/GNU-Linux-x86/Skippy.o.d
+++
@@ -1,5 +0,0 @@
-build/Release/GNU-Linux-x86/Skippy.o: Skippy.cpp Skippy.h Benchmark.h
-
-Skippy.h:
-
-Benchmark.h:
