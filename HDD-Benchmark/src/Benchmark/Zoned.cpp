a8a5a0e24cf79111f2da27f7a66bdbe1e160550e
Christian Deckert <c.deckert@me.com>
2014-03-16 20:25:33 +0100

    progress anzeige

--- HDD-Benchmark/src/Benchmark/Zoned.cpp
+++ HDD-Benchmark/src/Benchmark/Zoned.cpp
@@ -37,7 +37,7 @@ namespace Benchmark
 			{
 				//perror("read");
 				lap++;
-				std::cout << "\r" << iterations << ":" << lap << "\n";
+				std::cout << "\r" << round(lap/iterations*1000)/10 << "";
 				stopwatch.lap();
 				transfer = 0;
 			}
