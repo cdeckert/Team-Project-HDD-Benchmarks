6e48b37e916aca461bf156aa712ce6117b67e2e8
cdeckert <christian.deckert@zentrale2.com>
2014-04-16 20:56:20 +0200

    Prop reader

---
+++ HDD-Benchmark/src/HDDPropertyReader.cpp
@@ -0,0 +1,21 @@
+/*
+ * HDDPropertyReader.cpp
+ *
+ *  Created on: Apr 16, 2014
+ *      Author: cde
+ */
+
+#include "HDDPropertyReader.h"
+
+namespace HDDTest {
+
+HDDPropertyReader::HDDPropertyReader() {
+	// TODO Auto-generated constructor stub
+
+}
+
+HDDPropertyReader::~HDDPropertyReader() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace HDDTest */
---
+++ HDD-Benchmark/src/HDDPropertyReader.h
@@ -0,0 +1,21 @@
+/*
+ * HDDPropertyReader.h
+ *
+ *  Created on: Apr 16, 2014
+ *      Author: cde
+ */
+
+#ifndef HDDPROPERTYREADER_H_
+#define HDDPROPERTYREADER_H_
+
+namespace HDDTest {
+
+class HDDPropertyReader {
+public:
+	HDDPropertyReader();
+	virtual ~HDDPropertyReader();
+};
+
+} /* namespace HDDTest */
+
+#endif /* HDDPROPERTYREADER_H_ */
